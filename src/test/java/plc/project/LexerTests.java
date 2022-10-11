239cf5f02c93719b1c365c9c51a8ba576bd05fc6
drosabal <dmrosabal@gmail.com>
2022-10-11 06:31:19 -0400

    Added test cases to LexerTests.

--- src/test/java/plc/project/LexerTests.java
+++ src/test/java/plc/project/LexerTests.java
@@ -23,7 +23,9 @@ public class LexerTests {
                 Arguments.of("Alphabetic", "getName", true),
                 Arguments.of("Alphanumeric", "thelegend27", true),
                 Arguments.of("Leading Hyphen", "-five", false),
-                Arguments.of("Leading Digit", "1fish2fish3fishbluefish", false)
+                Arguments.of("Leading Digit", "1fish2fish3fishbluefish", false),
+                Arguments.of("Short Identifier", "a", true),
+                Arguments.of("Long Identifier", "abcdefghijklmnopqrstuvwxyz012346789_-", true)
         );
     }
 
@@ -38,7 +40,8 @@ public class LexerTests {
                 Arguments.of("Single Digit", "1", true),
                 Arguments.of("Multiple Digits", "12345", true),
                 Arguments.of("Negative", "-1", true),
-                Arguments.of("Leading Zero", "01", false)
+                Arguments.of("Leading Zero", "01", false),
+                Arguments.of("Zero", "0", true)
         );
     }
 
@@ -68,7 +71,8 @@ public class LexerTests {
                 Arguments.of("Alphabetic", "\'c\'", true),
                 Arguments.of("Newline Escape", "\'\\n\'", true),
                 Arguments.of("Empty", "\'\'", false),
-                Arguments.of("Multiple", "\'abc\'", false)
+                Arguments.of("Multiple", "\'abc\'", false),
+                Arguments.of("Unterminated Empty", "\'", false)
         );
     }
 
@@ -84,7 +88,8 @@ public class LexerTests {
                 Arguments.of("Alphabetic", "\"abc\"", true),
                 Arguments.of("Newline Escape", "\"Hello,\\nWorld\"", true),
                 Arguments.of("Unterminated", "\"unterminated", false),
-                Arguments.of("Invalid Escape", "\"invalid\\escape\"", false)
+                Arguments.of("Invalid Escape", "\"invalid\\escape\"", false),
+                Arguments.of("Alphabetic Escapes", "\"a\\bcdefghijklm\\nopq\\rs\\tuvwxyz\"", true)
         );
     }
 
@@ -125,6 +130,72 @@ public class LexerTests {
                         new Token(Token.Type.STRING, "\"Hello, World!\"", 6),
                         new Token(Token.Type.OPERATOR, ")", 21),
                         new Token(Token.Type.OPERATOR, ";", 22)
+                )),
+                Arguments.of("Example 3", "VAR i = -1 : Integer;\nVAL inc = 2 : " +
+                        "Integer;\nFUN foo() DO\n    WHILE i != 1 DO\n        IF i > 0 DO\n            " +
+                        "print(\"bar\");\n        END\n        i = i + inc;\n    END\nEND", Arrays.asList(
+                        //VAR i = -1 : Integer;
+                        new Token(Token.Type.IDENTIFIER, "VAR", 0),
+                        new Token(Token.Type.IDENTIFIER, "i", 4),
+                        new Token(Token.Type.OPERATOR, "=", 6),
+                        new Token(Token.Type.INTEGER, "-1", 8),
+                        new Token(Token.Type.OPERATOR, ":", 11),
+                        new Token(Token.Type.IDENTIFIER, "Integer", 13),
+                        new Token(Token.Type.OPERATOR, ";", 20),
+
+                        //VAL inc = 2 : Integer;
+                        new Token(Token.Type.IDENTIFIER, "VAL", 22),
+                        new Token(Token.Type.IDENTIFIER, "inc", 26),
+                        new Token(Token.Type.OPERATOR, "=", 30),
+                        new Token(Token.Type.INTEGER, "2", 32),
+                        new Token(Token.Type.OPERATOR, ":", 34),
+                        new Token(Token.Type.IDENTIFIER, "Integer", 36),
+                        new Token(Token.Type.OPERATOR, ";", 43),
+
+                        //DEF foo() DO
+                        new Token(Token.Type.IDENTIFIER, "FUN", 45),
+                        new Token(Token.Type.IDENTIFIER, "foo", 49),
+                        new Token(Token.Type.OPERATOR, "(", 52),
+                        new Token(Token.Type.OPERATOR, ")", 53),
+                        new Token(Token.Type.IDENTIFIER, "DO", 55),
+
+                        //    WHILE i != 1 DO
+                        new Token(Token.Type.IDENTIFIER, "WHILE", 62),
+                        new Token(Token.Type.IDENTIFIER, "i", 68),
+                        new Token(Token.Type.OPERATOR, "!=", 70),
+                        new Token(Token.Type.INTEGER, "1", 73),
+                        new Token(Token.Type.IDENTIFIER, "DO", 75),
+
+                        //        IF i > 0 DO
+                        new Token(Token.Type.IDENTIFIER, "IF", 86),
+                        new Token(Token.Type.IDENTIFIER, "i", 89),
+                        new Token(Token.Type.OPERATOR, ">", 91),
+                        new Token(Token.Type.INTEGER, "0", 93),
+                        new Token(Token.Type.IDENTIFIER, "DO", 95),
+
+                        //            print(\"bar\");
+                        new Token(Token.Type.IDENTIFIER, "print", 110),
+                        new Token(Token.Type.OPERATOR, "(", 115),
+                        new Token(Token.Type.STRING, "\"bar\"", 116),
+                        new Token(Token.Type.OPERATOR, ")", 121),
+                        new Token(Token.Type.OPERATOR, ";", 122),
+
+                        //        END
+                        new Token(Token.Type.IDENTIFIER, "END", 132),
+
+                        //        i = i + inc;
+                        new Token(Token.Type.IDENTIFIER, "i",144),
+                        new Token(Token.Type.OPERATOR, "=", 146),
+                        new Token(Token.Type.IDENTIFIER, "i", 148),
+                        new Token(Token.Type.OPERATOR, "+", 150),
+                        new Token(Token.Type.IDENTIFIER, "inc", 152),
+                        new Token(Token.Type.OPERATOR, ";", 155),
+
+                        //    END
+                        new Token(Token.Type.IDENTIFIER, "END", 161),
+
+                        //END
+                        new Token(Token.Type.IDENTIFIER, "END", 165)
                 ))
         );
     }
